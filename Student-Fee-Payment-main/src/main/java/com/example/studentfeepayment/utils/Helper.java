34bd1358280b1e91ec1fc0454d8da12b458a9fd1
Shreyank <shreyankbuddhadev99@gmail.com>
2021-12-26 14:12:42 +0530

    project

---
+++ Student-Fee-Payment-main/src/main/java/com/example/studentfeepayment/utils/Helper.java
@@ -0,0 +1,9 @@
+package com.example.studentfeepayment.utils;
+
+import com.example.studentfeepayment.bean.Bills;
+
+public class Helper {
+    public static void main(String args[]){
+        Bills b = new Bills();
+    }
+}
